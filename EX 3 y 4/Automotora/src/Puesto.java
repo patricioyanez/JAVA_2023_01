b4dbc0c5f3c154b477495516501c206950c9e289
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-01-10 11:44:57 -0300

    update

---
+++ EX 3 y 4/Automotora/src/Puesto.java
@@ -0,0 +1,38 @@
+
+public class Puesto {
+    private int codigo;
+    private String nombre;
+
+    public Puesto() {
+        this.codigo = 0;
+        this.nombre = "";
+    }
+    
+    public Puesto(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String imprimirDatos()
+    {
+        String datos = "\nCodigo    : " + this.codigo +
+                        "\nNombre   : " + this.nombre;
+        return datos;
+    }
+}
