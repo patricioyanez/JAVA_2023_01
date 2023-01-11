1548c11400d35aa118cb9c169b168155eb956029
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-01-11 12:04:56 -0300

    update

--- EX 3 y 4/Herencia/src/Anillo.java
+++ EX 3 y 4/Herencia/src/Anillo.java
@@ -1,4 +1,33 @@
 
-public class Anillo {
+public class Anillo extends Joya {
+    private float medida;
+
+    public Anillo() {
+        super();
+        this.medida = 0;
+    }
+    
+    public Anillo(float medida, int codigo, String nombre, String material) {
+        super(codigo, nombre, material);
+        this.medida = medida;
+    }
+
+    public float getMedida() {
+        return medida;
+    }
+
+    public void setMedida(float medida) {
+        this.medida = medida;
+    }
+
+    public String imprimirDatos() {
+        return "****** Anillo ******" + 
+               "\nCodigo    : " + super.getCodigo() +
+               "\nNombre    : " + super.getNombre() +
+               "\nMaterial  : " + super.getMaterial()+
+               "\nMedida    : " + this.medida // this.getLargo()
+                ;
+    }
+    
     
 }
