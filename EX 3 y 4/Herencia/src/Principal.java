7600e4f060fccbae73f69072d2eddb52401cc327
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-01-11 12:13:27 -0300

    update

--- EX 3 y 4/Herencia/src/Principal.java
+++ EX 3 y 4/Herencia/src/Principal.java
@@ -1,6 +1,17 @@
 
 public class Principal {
     public static void main(String[] args) {
+        Cadena cadena1 = new Cadena(45, 1000, "Deluxe", "Oro 14k");
+        Cadena cadena2 = new Cadena(60, 2500, "Super Deluxe", "Oro 14k");
         
+        cadena1.setMaterial("Oro 18k");        
+        System.out.println(cadena1.imprimirDatos());
+        
+        cadena2.setLargo(80);
+        cadena2.setMaterial("Oro 18k");
+        System.out.println(cadena2.imprimirDatos());
+        
+        
+        // crear 2 anillos e imprimir los datos que contienen
     }
 }
