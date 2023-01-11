aac7ca333b05f529e0225a7a1873b042baa4de36
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-01-11 12:45:29 -0300

    update

---
+++ EX 3 y 4/HerenciaOverLoadOverRide/src/Cadena.java
@@ -0,0 +1,36 @@
+
+public class Cadena extends Joya {
+    private int largo;
+    
+    public Cadena() {
+//        super();
+        this.largo = 0;
+    }
+    
+    public Cadena(int largo, int codigo, String nombre, String material) {
+        super(codigo, nombre, material);
+        this.largo = largo;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+
+    @Override
+    public String imprimirDatos() {
+        return "****** Cadena ******" + 
+               "\nCodigo    : " + super.getCodigo() +
+               "\nNombre    : " + super.getNombre() +
+               "\nMaterial  : " + super.getMaterial()+
+               "\nLargo     : " + this.largo // this.getLargo()
+                ;
+    }
+    
+    
+    
+}
---
+++ EX 3 y 4/HerenciaOverLoadOverRide/src/Principal.java
@@ -0,0 +1,8 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        Cadena cadena1 = new Cadena(45, 1000, "Deluxe", "Oro 14k");
+        
+        System.out.println(cadena1.imprimirDatos());
+    }
+}
