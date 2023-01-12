03305cdd7c225a75cfae5da7cca297ea7025aa6e
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-01-12 10:57:28 -0300

    update

--- EX 3 y 4/HerenciaOverLoadOverRide/src/Principal.java
+++ EX 3 y 4/HerenciaOverLoadOverRide/src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
         Cadena cadena1 = new Cadena(65, 1000, "Deluxe", "Oro 14k");
         
         System.out.println(cadena1.imprimirDatos());
-        
+        // sobrecarga de metodos
         System.out.println("Precio normal       : " + cadena1.precio());
         System.out.println("Precio 10% descuento: " + cadena1.precio(10));
         System.out.println("Precio en dolar     : " + cadena1.precio("dolar"));
