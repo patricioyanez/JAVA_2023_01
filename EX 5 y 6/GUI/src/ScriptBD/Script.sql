4f62d511c2fb0f39983257f0d28cc4f2f62df690
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-01-18 10:01:56 -0300

    Create Script.sql

---
+++ EX 5 y 6/GUI/src/ScriptBD/Script.sql
@@ -0,0 +1,14 @@
+CREATE TABLE AUTOMOVIL
+(
+    idAutomovil         int             primary key auto_increment,
+    patente             varchar(6)      not null,
+    marca               varchar(100)    not null,
+    modelo              varchar(100)    not null,
+    precioServicioNeto  int          not null,
+    valorIVA            int             not null,
+    totalAPagar         int             not null,
+    litrosMaletero      int             null,
+    cantidadPuerta      int             null,
+    largoManillar       int             null,
+    estilo              varchar(100)    null
+);
